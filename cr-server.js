b91af1abbd50ddec000a9613915cd48858ea19ff
kchresfield <kaelyn.chresfield@gmail.com>
2025-05-05 08:03:46 -0700

    separating wss and base server

--- cr-server.js
+++ cr-server.js
@@ -18,7 +18,7 @@ ExpressWs(app);
 
 ///////////////
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 ////// Firebase Stuff /////
@@ -81,7 +81,7 @@ app.post('/voice', async (req, res) => {
   console.log("here's the data: ", data)
 
   connect.conversationRelay({
-    url: "wss://6bd61a7d7052.ngrok.app/connect",
+    url: "wss://6bd61a7d7052.ngrok.app/connection",
     transcriptionProvider: "Google",
     ttsProvider: 'Elevenlabs',
     speechModel: "telephony",
@@ -180,109 +180,6 @@ app.post('/create-assistant', async (req, res) => {
   res.send(200, "good")
 
 })
-
-app.post('/look-up-assistant', async (req, res) => {
-  console.log("looking up an already created assistant")
-})
-
-app.post('/check-for-existing-assistant', async (req, res) => {
-  console.log("Checking forr an existing assistant")
-})
-
-
-
-
-app.ws('/connection', (ws) => {
-  try {
-
-    ws.on('message', async data => { // Incoming message from CR
-      const msg = JSON.parse(data);
-      console.log("Incoming orcestration: ", msg);
-
-      if (msg.type === "setup") {
-        // Find the threadID from firebase
-        const phoneNumber = msg.from.slice(2, msg.from.length) // remove the country code (+1)
-        let userId = hashPhoneNumber(phoneNumber);
-
-        // Preform query in firebase
-        let data;
-        try {
-          const userRef = ref(db, `users/${userId}/profile`);
-          const result = await get(userRef)
-          data = result.val();
-        } catch (err) {
-          console.error("Issue pulling user data from firebase: ", err)
-        }
-
-        console.log("here's the data: ", data)
-
-
-        // Conversation logic
-        if (data) {
-          ws.threadId = data.thread.id;
-          ws.assistantId = data.assistantId;
-
-          // Pretend the user starts the conversation
-          await openai.beta.threads.messages.create(ws.threadId, {
-            role: "user",
-            content: "Hi there!"
-          });
-
-
-          const run = openai.beta.threads.runs.stream(ws.threadId, {
-            assistant_id: ws.assistantId
-          });
-
-          run.on('textDone', async (textDone, snapshot) => {
-            // Send response from OpenAI model to Conversation Relay to be read back to the caller
-            console.log("textdone: ", textDone)
-
-            ws.send(
-              JSON.stringify({
-                type: "text",
-                token: textDone.value
-              })
-            )
-            // console.log("after")
-          })
-        }
-      } else if (msg.type === "prompt") { // A user begins speaking
-        // Add their question to the thread with the role "user"
-        console.log(ws.threadId)
-        let addMsgToThread = await openai.beta.threads.messages.create(ws.threadId, {
-          role: "user",
-          content: msg.voicePrompt
-        });
-
-        const run = openai.beta.threads.runs.stream(ws.threadId, {
-          assistant_id: ws.assistantId
-        })
-
-        run.on('textDone', (textDone, snapshot) => {
-          // Send response from OpenAI model to Conversation Relay to be read back to the caller
-          ws.send(
-            JSON.stringify({
-              type: "text",
-              token: textDone.value
-            })
-          )
-          console.log("textDone: ", textDone)
-        })
-        // Continue the conversation until hangup   
-      }
-    })
-    ws.on("close", async () => {
-      // delete assistant
-      // const response = await openai.beta.threads.del(thread1);// deelte the thread
-      console.log("WebSocket connection closed and OpenAI thread deleted");
-    });
-
-  } catch (err) {
-    console.error("ERROR: ", err);
-  }
-});
-
-
 
 
 app.listen(PORT, () => {
