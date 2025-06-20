67cdd75dac3fd64c4de4f8fe5d1b7734fa29f449
Andrew MacLean <amaclean@twilio.com>
2025-06-20 10:15:57 -0700

    Update for Railway

--- cr-server.js
+++ cr-server.js
@@ -19,8 +19,20 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 ////// Firebase Stuff /////
 import { getDatabase, ref, update, serverTimestamp, get } from "firebase/database";
 import admin from "firebase-admin";
-// import serviceAccount from "./service-account-key.json" assert {type: "json"}; // local
-import serviceAccount from "/etc/secrets/service-account-key.json" with { type: "json" }; // deployment
+
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -64,7 +76,7 @@ app.post('/voice', async (req, res) => {
   console.log("here's the data: ", data)
 
   connect.conversationRelay({
-    url: "wss://owlvin-ai-server-js-692351747341.us-west1.run.app/connection", //deployed > "wss://6bd61a7d7052.ngrok.app/connection", // dev
+    url: `wss://${process.env.WEBSOCKET_SERVER_URL}/connection`, // build from host env var
     ttsProvider: 'Elevenlabs',
     transcriptionProvider: "Deepgram",
     speechModel: "nova-3-general",
